20389c150cca9123ce2fb41fbf78838e5d7a1e5c
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2026-05-21 02:19:08 +0330

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() {
             println!("{remainder}")
         } else if command == "type" {
             if known_commands.contains(&&remainder[..]) {
-                println!("{command} is a shell builtin");
+                println!("{remainder} is a shell builtin");
             } else {
                 println!("{command}: not found");
             }
