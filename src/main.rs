3a7c0328e8220ad629fd0f6dc5a3b407ad083cab
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2026-05-21 02:13:57 +0330

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,18 @@ fn main() {
 
         std::io::stdin().read_line(&mut buffer).unwrap();
 
-        let command = buffer.trim();
+        let buffer = buffer.trim();
+        let line = &buffer.split(" ").collect::<Vec<&str>>();
+        let (command, remainder) = line.split_at(1);
+        let command = command[0];
+        let remainder = remainder.join(" ");
 
         if command == "exit" {
             return;
+        } else if command == "echo" {
+            println!("{remainder}")
+        } else {
+            println!("{command}: command not found");
         }
-
-        println!("{command}: command not found");
     }
 }
