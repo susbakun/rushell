965ec1797cb8580c9218fca6c848146f65be8782
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2026-05-22 15:52:13 +0330

    codecrafters submit [skip ci]

--- src/remainder.rs
+++ src/remainder.rs
@@ -25,13 +25,31 @@ pub fn parse_args(remainder: &str) -> Vec<String> {
 
 pub fn process_remainder(remainder: &str) -> String {
     let mut formated = String::new();
-    let mut is_quote = false;
+    let (mut is_single_quote, mut is_double_quote) = (false, false);
     let mut space_count = 0;
 
     for ch in remainder.chars() {
         if ch == '\'' || ch == '"' {
-            is_quote = !is_quote;
-        } else if is_quote {
+            if is_double_quote {
+                if ch == '"' {
+                    is_double_quote = !is_double_quote;
+                } else {
+                    formated.push(ch);
+                }
+            } else if is_single_quote {
+                if ch == '\'' {
+                    is_single_quote = !is_single_quote;
+                } else {
+                    formated.push(ch);
+                }
+            } else {
+                if ch == '\'' {
+                    is_single_quote = !is_single_quote;
+                } else {
+                    is_double_quote = !is_double_quote;
+                }
+            }
+        } else if is_double_quote || is_single_quote {
             formated.push(ch);
         } else if ch == ' ' {
             space_count += 1;
