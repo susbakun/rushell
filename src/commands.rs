dc8112fb0a65091906552c2ee8acf63ea5435007
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2026-05-22 03:33:48 +0330

    codecrafters submit [skip ci]

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-pub fn process_command(command: &str, remainder: &String, paths: &Vec<&str>) -> Result<()> {
+pub fn process_command(command: &str, remainder: &str, paths: &Vec<&str>) -> Result<()> {
     if command == "exit" {
         handle_exit_command();
     } else if command == "echo" {
@@ -14,15 +14,45 @@ pub fn process_command(command: &str, remainder: &String, paths: &Vec<&str>) -> 
     Ok(())
 }
 
+fn process_remainder(remainder: &str) -> String {
+    let mut formated = String::new();
+    let mut single_quote = false;
+    let mut space_count = 0;
+
+    for ch in remainder.chars() {
+        if ch == '\'' {
+            single_quote = !single_quote;
+            space_count = 0;
+        } else if single_quote {
+            formated.push(ch);
+            space_count = 0;
+        } else if ch == ' ' {
+            space_count += 1;
+            if space_count < 2 {
+                formated.push(ch);
+            }
+        } else {
+            formated.push(ch);
+            space_count = 0;
+        }
+    }
+
+    formated
+}
+
 fn handle_exit_command() {
     std::process::exit(0);
 }
 
-fn handle_echo_command(remainder: &String) {
+fn handle_echo_command(remainder: &str) {
+    let remainder = process_remainder(remainder);
+
     println!("{remainder}")
 }
 
-fn handle_type_command(remainder: &String, paths: &Vec<&str>) -> Result<()> {
+fn handle_type_command(remainder: &str, paths: &Vec<&str>) -> Result<()> {
+    let remainder = process_remainder(remainder);
+
     if KNOWN_COMMANDS.contains(&&remainder[..]) {
         println!("{remainder} is a shell builtin");
     } else {
@@ -39,7 +69,7 @@ fn handle_type_command(remainder: &String, paths: &Vec<&str>) -> Result<()> {
     Ok(())
 }
 
-fn handle_executable_command(remainder: &String, command: &str, paths: &Vec<&str>) -> Result<()> {
+fn handle_executable_command(remainder: &str, command: &str, paths: &Vec<&str>) -> Result<()> {
     let (_, found) = find_exe(paths, command)?;
     let args = remainder.split(" ").collect::<Vec<&str>>();
     if found {
--- src/main.rs
+++ src/main.rs
@@ -30,34 +30,7 @@ fn main() -> Result<()> {
 
         let buffer = buffer.trim();
         let (command, remainder) = &buffer.split_once(" ").unwrap();
-        let remainder = process_remainder(remainder);
 
-        process_command(command, &remainder, &paths)?;
+        process_command(command, remainder, &paths)?;
     }
-}
-
-fn process_remainder(remainder: &str) -> String {
-    let mut formated = String::new();
-    let mut single_quote = false;
-    let mut space_count = 0;
-
-    for ch in remainder.chars() {
-        if ch == '\'' {
-            single_quote = !single_quote;
-            space_count = 0;
-        } else if single_quote {
-            formated.push(ch);
-            space_count = 0;
-        } else if ch == ' ' {
-            space_count += 1;
-            if space_count < 2 {
-                formated.push(ch);
-            }
-        } else {
-            formated.push(ch);
-            space_count = 0;
-        }
-    }
-
-    formated
 }
