7de4acd53a8a8fcbdbc7b583bc1a2c46db6b9152
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2026-05-25 22:48:33 +0330

    codecrafters submit [skip ci]

--- src/commands.rs
+++ src/commands.rs
@@ -59,7 +59,7 @@ fn handle_executable_command(args: &[String], command: &str, paths: &Vec<&str>) 
 
         let stderr = String::from_utf8_lossy(&command_output.stderr);
         if !stderr.is_empty() {
-            return process_output(&stderr.to_string(), args, true);
+            process_output(&stderr.to_string(), args, true);
         }
 
         let output = format!("{}", String::from_utf8_lossy(&command_output.stdout));
