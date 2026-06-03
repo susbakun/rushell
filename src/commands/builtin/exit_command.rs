76448ef61d27b1ebf9ed96eaf6879556db3a495c
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2026-06-03 20:05:31 +0330

    codecrafters submit [skip ci]

--- src/commands/builtin/exit_command.rs
+++ src/commands/builtin/exit_command.rs
@@ -1,6 +1,6 @@
 use super::*;
 
 pub fn handle_exit_command(shell: &mut Shell) -> Result<()> {
-    shell.write_history_to_file(None, false)?;
+    let _ = shell.write_history_to_file(None, false);
     std::process::exit(0);
 }
