50b8c54a4983ce94b053facb133664ac927a43a2
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2026-06-05 16:06:31 +0330

    codecrafters submit [skip ci]

--- src/commands/builtin/cd_command.rs
+++ src/commands/builtin/cd_command.rs
@@ -5,14 +5,21 @@ use super::*;
 pub fn handle_cd_command(shell: &Shell, args: &[String]) -> Result<usize> {
     let segmented_args = segment_args(shell, args);
 
-    let path = &segmented_args[0];
-    let path = PathBuf::from(path);
+    let path_str = &segmented_args[0];
+    let mut path = PathBuf::from(path_str);
 
-    if !path.is_dir() {
+    if !path.is_dir() && !is_root_dir(path_str) {
         let output = format!("cd: {}: No such file or directory", path.display());
         return process_output(&output, args, true);
     } else {
+        if is_root_dir(path_str) {
+            path = std::env::home_dir().ok_or_else(|| anyhow!("home path is not specified"))?;
+        }
         env::set_current_dir(path)?;
         return Ok(0);
     }
+}
+
+fn is_root_dir(path_str: &String) -> bool {
+    path_str == "~"
 }
