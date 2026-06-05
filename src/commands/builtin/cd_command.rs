15b0b7a1b6582aba8a14a5a085a35fe92c61d4b0
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2026-06-05 15:58:54 +0330

    codecrafters submit [skip ci]

--- src/commands/builtin/cd_command.rs
+++ src/commands/builtin/cd_command.rs
@@ -8,7 +8,7 @@ pub fn handle_cd_command(shell: &Shell, args: &[String]) -> Result<usize> {
     let path = &segmented_args[0];
     let path = PathBuf::from(path);
 
-    if !path.starts_with("/") || !path.is_dir() {
+    if !path.is_dir() {
         let output = format!("cd: {}: No such file or directory", path.display());
         return process_output(&output, args, true);
     } else {
